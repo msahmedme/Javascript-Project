46beeb149baace62358eb2cf741666c8177e22fc
msahmedme <msabbirahmedse@gmail.com>
2022-10-08 21:50:53 +0600

    updating files

--- Calculator-Using-Javascript/JS/index.js
+++ Calculator-Using-Javascript/JS/index.js
@@ -1,8 +1,30 @@
-let number = $(".display").val()
+var resultField = $(".display");
 
-function insertNumber(inputValue){
+function insertNumber(number){
+    var existingNumber = resultField.val()
+    $(".display").val(existingNumber + number);
+}
 
-    number = number + inputValue
-    $(".display").val(number);
-    
+function clearResult(){
+    resultField.val('');
+}
+
+function del(){
+    var currentValue = resultField.val();
+    if(currentValue !=""){
+        resultField.val(resultField.val().slice(0, -1));
+    }
+}
+
+function calculate(){
+    var result = eval(resultField.val())
+    resultField.val('')
+    resultField.val(result)
+
+}
+
+function percentage(){
+    let number = resultField.val();
+
+    resultField.val(eval(number/100))
 }
